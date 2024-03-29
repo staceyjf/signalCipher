3ad17f4d7cc5880860a95c2486863169cccd0a9d
staceyjf <staceyfanner@gmail.com>
2024-03-29 15:29:43 +1100

    basic test add and revamp of html to reflect test set up

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,74 @@
 console.log("working");
 
-import { morseDictionary } from "./utlis.js";
+// import { morseDictionary } from "./utlis.js";
+
+// defining my morse code dictionary
+export const morseDictionary = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+  0: "-----",
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "'": ".----.",
+  "!": "-.-.--",
+  "/": "-..-.",
+  "(": "-.--.",
+  ")": "-.--.-",
+  "&": ".-...",
+  ":": "---...",
+  ";": "-.-.-.",
+  "=": "-...-",
+  "+": ".-.-.",
+  "-": "-....-",
+  _: "..--.-",
+  '"': ".-..-.",
+  $: "...-..-",
+  "@": ".--.-.",
+  "¿": "..-.-",
+  "¡": "--...-",
+};
+
+// more code rules
+// separator: words are separated by "/"
+// space: each character is separated by a single space
+
+const translateToEnglish = (event) => {
+  if (event.target.value === "morse") {
+  }
+};
+
+const translateToMorse = () => {};
--- tests/scripts.test.js
+++ tests/scripts.test.js
@@ -1,109 +1,163 @@
 // more code rules
 // separator: words are separated by "/"
-// space: each character is separated by a single space i
+// space: each character is separated by a single space
+
+const invalidInputError = new Error("A string has not been provided");
+const undefinedError = new Error("A match could not be found - undefined");
+const nullError = new Error("A match could not be found - null");
 
 // English to morse code
 // convert a single character to morse
 describe("test cases for a function that translates a single char to morse code", () => {
-  const nullOrUndefinedError = new Error(
-    "Invalid input: a null and an undefined parameter was provided."
-  );
-
   it("should return a translated a single char to a morse code string", () => {
-    expect(charToMorse("h")).toBe("....");
-    expect(charToMorse("w")).toBe(".--");
+    expect(translateToEnglish("h")).toBe("....");
+    expect(translateToEnglish("w")).toBe(".--");
+    expect(translateToEnglish("1")).toBe(".----");
+    expect(translateToEnglish("!")).toBe("-.-.--");
   });
 
-  it("should throw a 'The char cannot be found' Error if a null or undefined parameter is given", () => {
-    expect(() => {
-      charToMorse(null);
-    }).toThrow(nullOrUndefinedError);
+  it("should return the same result for upper and lower case letters", () => {
+    expect(translateToEnglish("z")).toEqual(translateToEnglish("Z"));
   });
 
-  it("should throw a 'The char cannot be found' Error if a null or undefined parameter is given", () => {
+  it("should replace characters that can't be translated with a #", () => {
+    expect(translateToEnglish("<")).toBe("#");
+    expect(translateToEnglish("#")).toBe("#");
+  });
+
+  it("should return an empty string when the input is an empty string", () => {
+    expect(translateToEnglish("").toBe(""));
+  });
+
+  it("should correctly translate spaces", () => {
+    expect(translateToEnglish(" ")).toBe("/");
+  });
+
+  it("should throw an error when the input is not a string", () => {
+    expect(() => translateToEnglish(3)).toThrow(invalidInputError);
+    expect(() => translateToEnglish(true)).toThrow(invalidInputError);
+    expect(() => translateToEnglish({})).toThrow(invalidInputError);
+    expect(() => translateToEnglish([])).toThrow(invalidInputError);
+  });
+
+  it("should throw a 'The char cannot be be found' Error if a null parameter is given", () => {
     expect(() => {
-      charToMorse(undefined);
-    }).toThrow(nullOrUndefinedError);
+      translateToEnglish(null);
+    }).toThrow(nullError);
+  });
+
+  it("should throw a 'The char cannot be found' Error if an undefined parameter is given", () => {
+    expect(() => {
+      translateToEnglish(undefined);
+    }).toThrow(undefinedError);
+  });
+});
+
+// Morse code to english
+// convert a single code pattern to a english char
+describe("test cases for a function that translate a morse code letter to a single english character", () => {
+  it("should return a translated a single morse code block to a char", () => {
+    expect(translateToMorse("....")).toBe("H");
+    expect(translateToMorse(".--")).toBe("W");
+    expect(translateToMorse("--...")).toBe("7");
+    expect(translateToMorse("..--..")).toBe("?");
+  });
+
+  it("should return an empty string when the input is an empty string", () => {
+    expect(translateToMorse("").toBe(""));
+  });
+
+  it("should correctly translate a ' ' to be a new english char", () => {
+    expect(translateToMorse(" ")).toBe("");
+  });
+
+  it("should correctly translate a " / " to a space", () => {
+    expect(translateToMorse("/")).toBe(" ");
+  });
+
+  it("should throw an error when the input is not a string", () => {
+    expect(() => translateToMorse(3)).toThrow(invalidInputError);
+    expect(() => translateToMorse(true)).toThrow(invalidInputError);
+    expect(() => translateToMorse({})).toThrow(invalidInputError);
+    expect(() => translateToMorse([])).toThrow(invalidInputError);
   });
 });
 
 // convert a sentance to morse code
-describe("test cases for a function that returns a translated string", () => {
-  const invalidInputError = new Error(
-    "Invalid input: function expects a string parameter."
-  );
+// describe("test cases for a function that returns a translated string", () => {
+//   const invalidInputError = new Error(
+//     "Invalid input: function expects a string parameter."
+//   );
 
-  const undefinedError = new Error("A match could not be found - undefined");
-  const nullError = new Error("A match could not be found - null");
+//   const undefinedError = new Error("A match could not be found - undefined");
+//   const nullError = new Error("A match could not be found - null");
 
-  it("should return a morse code pattern", () => {
-    expect(translateToMorse("hello")).toBe(".... . .-.. .-.. ---");
-    expect(translateToMorse("world")).toBe(".-- --- .-. .-.. -..");
-  });
+//   it("should return a morse code pattern", () => {
+//     expect(translateToMorse("hello")).toBe(".... . .-.. .-.. ---");
+//     expect(translateToMorse("world")).toBe(".-- --- .-. .-.. -..");
+//   });
 
-  it("should return a translated english sentance to a morse code string", () => {
-    expect(charToMorse("hello world")).toBe(
-      ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."
-    );
-    expect(charToMorse("i love morse code")).toBe(
-      ".. / .-.. --- ...- . / -- --- .-. ... . / -.-. --- -.. ."
-    );
-  });
+//   it("should return a translated english sentance to a morse code string", () => {
+//     expect(translateToMorse("hello world")).toBe(
+//       ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."
+//     );
+//     expect(translateToMorse("i love morse code")).toBe(
+//       ".. / .-.. --- ...- . / -- --- .-. ... . / -.-. --- -.. ."
+//     );
+//   });
 
-  it("should return a translated numbers to a morse code string", () => {
-    expect(charToMorse("2024")).toBe("..--- ----- ..--- ....-");
-    expect(charToMorse("2012")).toBe("..--- ----- .---- ..---");
-  });
+//   it("should return a translated numbers to a morse code string", () => {
+//     expect(translateToMorse("2024")).toBe("..--- ----- ..--- ....-");
+//     expect(translateToMorse("2012")).toBe("..--- ----- .---- ..---");
+//   });
 
-  it("should return a translated numbers to a morse code string", () => {
-    expect(charToMorse("/!?")).toBe("-..-. -.-.-- ..--..");
-    expect(charToMorse("$$&&")).toBe("...-..- ...-..- .-... .-...");
-  });
+//   it("should return a translated numbers to a morse code string", () => {
+//     expect(translateToMorse("/!?")).toBe("-..-. -.-.-- ..--..");
+//     expect(translateToMorse("$$&&")).toBe("...-..- ...-..- .-... .-...");
+//   });
 
-  it("should return a translated mixed letters, numbers and special characters to a morse code string", () => {
-    expect(charToMorse("I am 21")).toBe(".. / .- -- / ..--- .----");
-    expect(charToMorse("whoo whoo!!! ")).toBe(
-      ".-- .... --- --- / .-- .... --- --- -.-.-- -.-.-- -.-.--"
-    );
-  });
+//   it("should return a translated mixed letters, numbers and special characters to a morse code string", () => {
+//     expect(translateToMorse("I am 21")).toBe(".. / .- -- / ..--- .----");
+//     expect(translateToMorse("whoo whoo!!! ")).toBe(
+//       ".-- .... --- --- / .-- .... --- --- -.-.-- -.-.-- -.-.--"
+//     );
+//   });
 
-  it("should return the same result for upper and lower case letters", () => {
-    const morseForHello = ".... . .-.. .-.. ---";
-    expect(charToMorse("hello")).toBe(morseForHello);
-    expect(charToMorse("HELLO")).toBe(morseForHello);
-  });
+//   it("should return the same result for upper and lower case letters", () => {
+//     const morseForHello = ".... . .-.. .-.. ---";
+//     expect(translateToMorse("hello")).toBe(morseForHello);
+//     expect(translateToMorse("HELLO")).toBe(morseForHello);
+//   });
 
-  it("should replace characters that can't be translated with a #", () => {
-    expect(charToMorse("<tag>")).toBe("# - .- --. #");
-    expect(charToMorse("#id")).toBe("# .. -..");
-  });
+//   it("should replace characters that can't be translated with a #", () => {
+//     expect(translateToMorse("<tag>")).toBe("# - .- --. #");
+//     expect(translateToMorse("#id")).toBe("# .. -..");
+//   });
 
-  it("should return an empty string when the input is an empty string", () => {
-    expect(charToMorse("").toBe(""));
-  });
+//   it("should return an empty string when the input is an empty string", () => {
+//     expect(translateToMorse("").toBe(""));
+//   });
 
-  it("should correctly translate spaces", () => {
-    expect(charToMorse(" ")).toBe("/");
-  });
+//   it("should correctly translate spaces", () => {
+//     expect(translateToMorse(" ")).toBe("/");
+//   });
 
-  it("should ignore leading and trailing whitespace", () => {
-    expect(charToMorse(" hello ")).toBe(".... . .-.. .-.. ---");
-  });
+//   it("should ignore leading and trailing whitespace", () => {
+//     expect(translateToMorse(" hello ")).toBe(".... . .-.. .-.. ---");
+//   });
 
-  it("should throw an error when the input is not a string", () => {
-    expect(() => charToMorse(123)).toThrow(invalidInputError);
-    expect(() => charToMorse(true)).toThrow(invalidInputError);
-    expect(() => charToMorse({})).toThrow(invalidInputError);
-    expect(() => charToMorse([])).toThrow(invalidInputError);
-  });
+//   it("should throw an error when the input is not a string", () => {
+//     expect(() => translateToMorse(123)).toThrow(invalidInputError);
+//     expect(() => translateToMorse(true)).toThrow(invalidInputError);
+//     expect(() => translateToMorse({})).toThrow(invalidInputError);
+//     expect(() => translateToMorse([])).toThrow(invalidInputError);
+//   });
 
-  it("should throw an error when the input is not a string", () => {
-    expect(() => charToMorse(null)).toThrow(nullError);
-  });
+//   it("should throw an error when the input is not a string", () => {
+//     expect(() => translateToMorse(null)).toThrow(nullError);
+//   });
 
-  it("should throw an error when the input is undefined", () => {
-    expect(() => charToMorse(undefined)).toThrow(undefinedError);
-  });
-});
-
-//
+//   it("should throw an error when the input is undefined", () => {
+//     expect(() => translateToMorse(undefined)).toThrow(undefinedError);
+//   });
+// });
