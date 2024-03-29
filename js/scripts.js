31f9256addbe660af84dbd008f78436f1a926515
staceyjf <staceyfanner@gmail.com>
2024-03-29 15:55:36 +1100

    initial js adds

--- js/scripts.js
+++ js/scripts.js
@@ -65,10 +65,40 @@ export const morseDictionary = {
 // more code rules
 // separator: words are separated by "/"
 // space: each character is separated by a single space
+const invalidInputError = new Error("A string has not been provided");
+const undefinedError = new Error("A match could not be found - undefined");
+const nullError = new Error("A match could not be found - null");
+
+const toMorseBtn = document.getElementById("toMorseBtn");
+const toEnglishBtn = document.getElementById("toEnglishBtn");
+let translatedResults = document.querySelectorAll("#translator__results");
 
 const translateToEnglish = (event) => {
-  if (event.target.value === "morse") {
-  }
+  let morseChar = "";
+
+  if (event.key === " ") morseChar = "/";
+
+  try{}.catch{}.finally{};
+
+  console.log(morseChar);
+ 
 };
 
 const translateToMorse = () => {};
+
+document.querySelectorAll("textarea").forEach((input) => {
+  input.addEventListener("keydown", (event) => {
+    // console.log(event.target.id); textarea id
+    // console.log(event.target.value); chars
+
+    if (event.target.id === "englishInput") {
+      translateToEnglish(event);
+    } else if (event.target.id === "morseInput") {
+      translateToMorse(event);
+    } else {
+      throw new Error(
+        "There has been an problem with translating please try again"
+      );
+    }
+  });
+});
