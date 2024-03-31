c15b4cc24f270073dd44efaf745ac2ca48576516
staceyjf <staceyfanner@gmail.com>
2024-03-31 13:42:38 +1100

    refactor of eventlistner

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,53 @@
 import { translateToEnglish, translateToMorse } from "./translate.js";
 
 // NODES
-const toMorseBtn = document.getElementById("toMorseBtn");
-const toEnglishBtn = document.getElementById("toEnglishBtn");
-const translationEl = document.getElementById("decodedOutput");
-let outputResult = "";
+// const toMorseBtn = document.getElementById("toMorseBtn");
+// const toEnglishBtn = document.getElementById("toEnglishBtn");
+const decodedOutputElement = document.getElementById("decodedOutput");
+let ongoingTranslatedText = "";
+
+//helper function
+function handleBackspace() {
+  // need to deal with the extra " " added by translatetoEnglish()
+  if (outputResult.trimEnd().includes(" ")) {
+    let lastSpaceIndex = outputResult.trimEnd().lastIndexOf(" ");
+    outputResult = outputResult.slice(0, lastSpaceIndex);
+  } else {
+    outputResult = "";
+  }
+  return (decodedOutputElement.textContent = outputResult);
+}
 
 document.querySelectorAll("textarea").forEach((input) => {
   input.addEventListener("keyup", (event) => {
-    translationEl.textContent = "";
+    decodedOutputElement.textContent = "";
+    let translatedChar = "";
 
     if (typeof event.key !== "string")
       throw new Error("Invalid input for the translating functions");
-
-    let outputChar = "";
 
     // if the user uses an english char that needs a "shift" ignore the shift char
     if (event.key === "Shift") return;
 
     try {
-      if (event.target.id === "englishInput") {
-        if (event.key === "Backspace") {
-          // need to deal with the extra " " added by translatetoEnglish()
-          if (outputResult.trimEnd().includes(" ")) {
-            let lastSpaceIndex = outputResult.trimEnd().lastIndexOf(" ");
-            outputResult = outputResult.slice(0, lastSpaceIndex);
-          } else {
-            outputResult = "";
+      switch (event.target.id) {
+        case "englishInput":
+          if (event.key === "Backspace") {
+            handleBackspace();
+            return; // Avoid sending Backspace to the translate functions
           }
-          translationEl.textContent = outputResult;
-          return; // we need to exit to avoid sending Backspace to the translate functions
-        }
-
-        outputChar = translateToEnglish(event.key);
+          translatedChar = translateToEnglish(event.key);
+          break;
+        case "morseInput":
+          translatedChar = translateToMorse(event.key);
+          break;
+        default:
+          console.error("Invalid target id");
+          return;
       }
-
-      if (event.target.id === "morseInput")
-        outputChar = translateToMorse(event.key);
-
-      // create the relevant dom elements
-      outputResult += outputChar;
-
-      //if the user uses the "backspace" delete the the output char that was just inputted
-
-      translationEl.textContent = outputResult;
+      // Create the relevant DOM elements
+      ongoingTranslatedText += translatedChar;
+      decodedOutputElement.textContent = ongoingTranslatedText;
     } catch (error) {
       console.error("ERROR: " + error);
       throw new Error(
@@ -52,7 +56,3 @@ document.querySelectorAll("textarea").forEach((input) => {
     }
   });
 });
-
-//   console.log(event.target.id); //textarea id
-//   console.log(event.target.value); //chars
-// console.log(event.key); //keys
